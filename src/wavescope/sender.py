0edcd85131540f8d12e3b2e0a6a664981e1086f7
saltchicken <john.eicher89@gmail.com>
2025-05-26 13:11:24 -0700

    Remove test

--- src/wavescope/sender.py
+++ src/wavescope/sender.py
@@ -12,22 +12,3 @@ class Publisher:
         # TODO: Test with localhost
         self.publisher.bind(f"tcp://*:{port}")
         self.bridge_task = asyncio.create_task(bridge.start_server())
-
-async def test(publisher):
-    while True:
-        freqs = np.linspace(0, 24000, 1024, dtype=np.float32)
-        mags = np.abs(np.random.randn(1024)).astype(np.float32)
-
-        # Concatenate freq and mag arrays into single bytes object
-        data = np.concatenate((freqs, mags)).tobytes()
-
-        publisher.send(data)
-        await asyncio.sleep(1 / 60)  # ~60 Hz publishing rate
-
-async def main():
-    publisher = Publisher()
-    test_task = asyncio.create_task(test(publisher.publisher))
-    await asyncio.gather(publisher.bridge_task, test_task)
-
-if __name__ == "__main__":
-    asyncio.run(main())
